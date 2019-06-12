a710d4f28ba464b8f8c5fd2c5c0bff454b901e20
Danny Lee <dreamer@panix.com>
2019-06-12 23:10:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
-    cart.push({itemName:item, itemPrice:Math.floor(Math.random()*100 + 1) )})
+    cart.push({itemName:item, itemPrice:Math.floor(Math.random()*100 + 1) });
     return console.log(`${item} has been added to your cart.`);
 }
 
