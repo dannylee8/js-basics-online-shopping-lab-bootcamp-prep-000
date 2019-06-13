4e8c8716be4a6c6e45aabcb649fe5e86c53eccd4
Danny Lee <dreamer@panix.com>
2019-06-13 01:04:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,8 +56,9 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  return "That items is not in your cart.";
+  return "That item is not in your cart.";
 }
+
 
 function placeOrder(cardNumber) {
   // write your code here
