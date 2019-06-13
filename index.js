c1b212ea8df5bd359389639afa7563b97ed3fdb8
Danny Lee <dreamer@panix.com>
2019-06-13 01:05:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
-      cart.splice(i+1, 1);
+      cart.splice(i, 1);
       return cart;
     }
   }
