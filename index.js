f1065600d55f24a584d72348b43f1b203b1e7d8f
Danny Lee <dreamer@panix.com>
2019-06-13 01:00:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,8 +51,11 @@ function total() {
 function removeFromCart(item) {
   // write your code here
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) cart.splice(i+1, )
+    if (cart[i].itemName === item) {
+      cart.splice(i+1, 1);
+      return cart;
   }
+  return "That items is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
